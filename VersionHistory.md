0f329edfea3dc492da75d66832bd0d795dbbbd5a
andrewstef <andrewstef@vt.edu>
2025-11-25 17:54:42 -0500

    added ability to die, count score correctly, press r to restart game after death

--- VersionHistory.md
+++ VersionHistory.md
@@ -1,4 +1,4 @@
 ## [1.0.0] - 2025-11-25
 - Iniital release
-- Includes basic game mechanics
-- Does not include the ability to die. Score does not increment properly
+- Created react app
+- Implemented basic game logic
--- flappy-bird/src/Game/FlappyBirdLogic.js
+++ flappy-bird/src/Game/FlappyBirdLogic.js
@@ -1,7 +1,21 @@
-import { BIRD_WIDTH_PX } from './Constants';
+import { BIRD_WIDTH_PX, PIPE_GAP_PX, PIPE_GENERATION_INTERVAL_FRAMES } from './Constants';
 import { Bird, Pipe } from './FlappyBirdObjects';
 
 export const RunFlappyBird = (canvas, SetScore) => {
+
+  const IsBirdAlive = () => {
+    for (let pipe of pipes)
+    {
+      // Check for collision between bird and pipe
+      if (bird.Within(pipe.x, pipe.y, pipe.width, pipe.gapY) ||
+          bird.Within(pipe.x, pipe.gapY + PIPE_GAP_PX, pipe.width, pipe.height - pipe.gapY - PIPE_GAP_PX))
+      {
+        return false;
+      }
+    }
+
+    return true;
+  }
 
   const UpdatePipes = () => {
     // Update each pipe
@@ -10,38 +24,71 @@ export const RunFlappyBird = (canvas, SetScore) => {
     // Remove pipes that have moved off screen
     pipes = pipes.filter(pipe => pipe.x + pipe.width > 0);
 
-    // Generate new pipes at intervals. Place at the right edge of the canvas.
-    if (frameCount % 100 === 0) {
+    // Generate new pipe on the first frame and at intervals. Place at the right edge of the canvas.
+    if (frameCount % PIPE_GENERATION_INTERVAL_FRAMES === 0 || frameCount === 0)
+    {
       let pipe = new Pipe(canvas.height, canvas.width, 0); // height, x, y
       pipes.push(pipe);
     }
   };
 
   const HandleKeyPress = (e) => {
-    if (e.key === ' ') {
-      //console.log('Space bar pressed'); // Debugging statement
+    if (e.key === ' ')
+    {
       bird.Fly();
       gameStarted = true; // Start the game when the space bar is pressed
+    }
+    else if (e.key === 'r' && gameOver)
+    {
+      // Reset the game
+      console.log("Resetting game.");
+      bird.SetPosition(50, canvas.height / 2 - (bird.height / 2));
+      pipes = [];
+      frameCount = 0;
+      scoreFrameCount = 0;
+      passedFirstPipe = false;
+      gameOver = false;
+      gameStarted = false;
+      SetScore(0);
     }
   };
 
   const GameLoop = () => {
     context.clearRect(0, 0, canvas.width, canvas.height);
     //console.log("Game Loop + ", gameStarted);
-    if (gameStarted)
+    if (gameStarted && !gameOver)
     {
       // Update objects every frame
       bird.Update();
       UpdatePipes();
+
+      // We have passed the first pipe if feft edge of the bird has passed right edge of first pipe
+      if (pipes.length > 0 && !passedFirstPipe)
+      {
+        passedFirstPipe = bird.x > pipes[0].x + pipes[0].width;
+      }
+
+      // Check if bird is still alive
+      if (IsBirdAlive())
+      {
+        if (passedFirstPipe)
+        {
+          SetScore(Math.floor((scoreFrameCount / PIPE_GENERATION_INTERVAL_FRAMES) + 1));
+          scoreFrameCount++;
+        }
+      }
+      else
+      {
+        console.log("Bird has collided with a pipe. Game Over.");
+        gameOver = true;
+      }
     }
 
-    // Draw objects
+    // Draw objects every frame
     bird.Draw(context);
     pipes.forEach(pipe => { pipe.Draw(context); });
 
-
     frameCount++;
-    SetScore(Math.floor(frameCount / 100));
     requestAnimationFrame(GameLoop);
   };
 
@@ -54,10 +101,14 @@ export const RunFlappyBird = (canvas, SetScore) => {
   let bird = new Bird();
   let pipes = [];
   let frameCount = 0;
+  let scoreFrameCount = 0;
   let gameStarted = false;
+  let passedFirstPipe = false;
+  let gameOver = false;
 
   // Set initial bird size once the image loads based on its image aspect ratio
-  bird.image.onload = () => {
+  bird.image.onload = () =>
+  {
     let imageAspectRatio = bird.image.naturalWidth / bird.image.naturalHeight;
     bird.SetWidthHeight(BIRD_WIDTH_PX, BIRD_WIDTH_PX / imageAspectRatio);
 
@@ -74,7 +125,8 @@ export const RunFlappyBird = (canvas, SetScore) => {
   window.addEventListener('keydown', HandleKeyPress);
   GameLoop(); // Start the game loop
 
-  return () => {
+  return () =>
+  {
     window.removeEventListener('keydown', HandleKeyPress);
   };
 };
