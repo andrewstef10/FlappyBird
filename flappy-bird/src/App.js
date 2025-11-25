df57915eb1d944d94284ad862ad0ca408875b29a
andrewstef <andrewstef@vt.edu>
2025-11-25 15:58:53 -0500

    add footer with version number and date

--- flappy-bird/src/App.js
+++ flappy-bird/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Game from './components/Game';
+import packageJson from '../package.json';
 
 function App() {
   return (
     <div className="App">
       <Game />
+      <footer style={{marginTop: '2rem', fontSize: '0.9rem', color: '#888'}}>
+        Version: {packageJson.version} &mdash; {new Date().toLocaleDateString()}
+      </footer>
     </div>
   );
 }
